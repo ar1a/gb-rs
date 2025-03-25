b25162909e53d4b7b9d46d10f153a73bf85dcdfb
Aria Edmonds <aria@ar1as.space>
2025-03-25 14:37:22 +0800

    extract registers out to module

---
+++ src/cpu/registers.rs
@@ -0,0 +1,67 @@
+use enumflags2::{BitFlag as _, BitFlags, bitflags};
+
+/// Base registers
+#[derive(Debug, Default)]
+pub(super) struct Registers {
+    pub(super) a: u8,
+    pub(super) b: u8,
+    pub(super) c: u8,
+    pub(super) d: u8,
+    pub(super) e: u8,
+    pub(super) h: u8,
+    pub(super) l: u8,
+
+    pub(super) f: BitFlags<Flags>,
+}
+
+#[bitflags]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub(super) enum Flags {
+    /// Set if the result of an operation is zero
+    Zero = 0b1000_0000,
+    /// Set if previous instruction was a Subtraction
+    Subtraction = 0b0100_0000,
+    /// Set if lower 4 bits carried over to upper 4 bits
+    HalfCarry = 0b0010_0000,
+    /// Set when something overflows
+    Carry = 0b0001_0000,
+}
+
+impl Registers {
+    pub(super) const fn bc(&self) -> u16 {
+        u16::from_le_bytes([self.b, self.c])
+    }
+    pub(super) const fn set_bc(&mut self, value: u16) {
+        let [b, c] = value.to_le_bytes();
+        self.b = b;
+        self.c = c;
+    }
+
+    pub(super) fn af(&self) -> u16 {
+        u16::from_le_bytes([self.a, self.f.bits()])
+    }
+    pub(super) fn set_af(&mut self, value: u16) {
+        let [a, f] = value.to_le_bytes();
+        self.a = a;
+        self.f = Flags::from_bits(f).unwrap();
+    }
+
+    pub(super) const fn de(&self) -> u16 {
+        u16::from_le_bytes([self.d, self.e])
+    }
+    pub(super) const fn set_de(&mut self, value: u16) {
+        let [d, e] = value.to_le_bytes();
+        self.d = d;
+        self.e = e;
+    }
+
+    pub(super) const fn hl(&self) -> u16 {
+        u16::from_le_bytes([self.h, self.l])
+    }
+    pub(super) const fn set_hl(&mut self, value: u16) {
+        let [h, l] = value.to_le_bytes();
+        self.h = h;
+        self.l = l;
+    }
+}
