a8ee7df81b6f3ef1fbd788b1c85726fb62228550
Aria Edmonds <aria@ar1as.space>
2025-03-25 23:06:50 +0800

    stub out IO memory map

--- src/cpu/memorybus.rs
+++ src/cpu/memorybus.rs
@@ -1,9 +1,16 @@
 use crate::gpu::{Gpu, VRAM_BEGIN, VRAM_END};
 
+pub const IO_BEGIN: usize = 0xFF00;
+pub const IO_END: usize = 0xFF7F;
+pub const IO_SIZE: usize = IO_END - IO_BEGIN + 1;
+
 #[derive(Debug)]
 pub(crate) struct MemoryBus {
     memory: [u8; 0xFFFF],
     gpu: Gpu,
+
+    // TODO: Implement io as a struct
+    io: [u8; IO_SIZE],
 }
 
 impl Default for MemoryBus {
@@ -11,6 +18,8 @@ impl Default for MemoryBus {
         Self {
             memory: [0; 0xFFFF],
             gpu: Gpu::default(),
+
+            io: [0; IO_SIZE],
         }
     }
 }
@@ -20,6 +29,7 @@ impl MemoryBus {
         let address = address as usize;
         match address {
             VRAM_BEGIN..=VRAM_END => self.gpu.read_vram(address - VRAM_BEGIN),
+            IO_BEGIN..=IO_END => self.io[address - IO_BEGIN],
             _ => todo!("memory region not mapped yet: {:#4x}", address),
         }
     }
@@ -27,6 +37,7 @@ impl MemoryBus {
         let address = address as usize;
         match address {
             VRAM_BEGIN..=VRAM_END => self.gpu.write_vram(address - VRAM_BEGIN, value),
+            IO_BEGIN..=IO_END => self.io[address - IO_BEGIN] = value,
             _ => todo!("memory region not mapped yet: {:#4x}", address),
         }
     }
