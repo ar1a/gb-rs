0c38274eefaac1095a6317f56d204e7a63f3d508
Aria Edmonds <aria@ar1as.space>
2025-03-25 05:07:53 +0000

    implement 16bit virtual registers

--- src/cpu.rs
+++ src/cpu.rs
@@ -20,4 +20,31 @@ impl Registers {
         self.b = b;
         self.c = c;
     }
+
+    const fn af(&self) -> u16 {
+        u16::from_le_bytes([self.a, self.f])
+    }
+    const fn set_af(&mut self, value: u16) {
+        let [a, f] = value.to_le_bytes();
+        self.a = a;
+        self.f = f;
+    }
+
+    const fn de(&self) -> u16 {
+        u16::from_le_bytes([self.d, self.e])
+    }
+    const fn set_de(&mut self, value: u16) {
+        let [d, e] = value.to_le_bytes();
+        self.d = d;
+        self.e = e;
+    }
+
+    const fn hl(&self) -> u16 {
+        u16::from_le_bytes([self.h, self.l])
+    }
+    const fn set_hl(&mut self, value: u16) {
+        let [h, l] = value.to_le_bytes();
+        self.h = h;
+        self.l = l;
+    }
 }
