96ea241f8b8ea8d81c0e573d43c2b4d6a392f102
Aria Edmonds <aria@ar1as.space>
2025-03-25 13:55:50 +0800

    implement ADD instruction

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use enumflags2::{BitFlag as _, BitFlags, bitflags};
 
 /// Base registers
@@ -18,9 +19,13 @@ struct Registers {
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, PartialEq)]
 enum Flags {
+    /// Set if the result of an operation is zero
     Zero = 0b1000_0000,
+    /// Set if previous instruction was a Subtraction
     Subtraction = 0b0100_0000,
+    /// Set if lower 4 bits carried over to upper 4 bits
     HalfCarry = 0b0010_0000,
+    /// Set when something overflows
     Carry = 0b0001_0000,
 }
 
@@ -59,5 +64,52 @@ impl Registers {
         let [h, l] = value.to_le_bytes();
         self.h = h;
         self.l = l;
+    }
+}
+
+enum Instruction {
+    Add(ArithmeticTarget),
+}
+
+enum ArithmeticTarget {
+    A,
+    B,
+    C,
+    D,
+    E,
+    H,
+    L,
+}
+
+struct Cpu {
+    registers: Registers,
+}
+
+impl Cpu {
+    fn execute(&mut self, instruction: Instruction) {
+        match instruction {
+            Instruction::Add(target) => match target {
+                ArithmeticTarget::C => {
+                    let value = self.registers.c;
+                    let new_value = self.add(value);
+                    self.registers.a = new_value;
+                }
+                _ => todo!("support more targets"),
+            },
+        }
+    }
+
+    fn add(&mut self, value: u8) -> u8 {
+        let (new_value, overflow) = self.registers.a.overflowing_add(value);
+        let flags = &mut self.registers.f;
+        flags.set(Flags::Zero, new_value == 0);
+        flags.remove(Flags::Subtraction);
+        flags.set(Flags::Carry, overflow);
+        // HalfCarry is set if the lower 4 bits added together don't fit in the lower 4 bits
+        flags.set(
+            Flags::HalfCarry,
+            (self.registers.a & 0b1111) + (value & 0b1111) > 0b1111,
+        );
+        new_value
     }
 }
