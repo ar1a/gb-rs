9166a1fdc6efaaf9f6f8528b6cd928e32935fea9
Aria Edmonds <aria@ar1as.space>
2025-03-28 18:37:01 +0800

    gpu add tile_set

--- src/gpu.rs
+++ src/gpu.rs
@@ -3,15 +3,30 @@ pub const VRAM_BEGIN: usize = 0x8000;
 pub const VRAM_END: usize = 0x9FFF;
 pub const VRAM_SIZE: usize = VRAM_END - VRAM_BEGIN + 1;
 
+#[derive(Debug, Clone, Copy)]
+enum TilePixelValue {
+    Zero,
+    One,
+    Two,
+    Three,
+}
+
+type Tile = [[TilePixelValue; 8]; 8];
+const fn empty_tile() -> Tile {
+    [[TilePixelValue::Zero; 8]; 8]
+}
+
 #[derive(Debug)]
 pub struct Gpu {
     vram: [u8; VRAM_SIZE],
+    tile_set: [Tile; 384],
 }
 
 impl Default for Gpu {
     fn default() -> Self {
         Self {
             vram: [0; VRAM_SIZE],
+            tile_set: [empty_tile(); 384],
         }
     }
 }
