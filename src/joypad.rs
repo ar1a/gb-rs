f9a2d9e1e84048f3514bbf067ed1c9aa11868b63
Aria Edmonds <aria@ar1as.space>
2025-04-28 13:46:38 +0800

    use bilge for joypad buttons/dpad selection, fix field order

--- src/joypad.rs
+++ src/joypad.rs
@@ -3,49 +3,72 @@ use bilge::prelude::*;
 #[allow(clippy::struct_excessive_bools)]
 #[derive(Debug, Clone, Copy, Default)]
 pub struct Joypad {
-    pub register: u8,
+    pub input_select: UpperNibble,
 
     pub buttons: Buttons,
     pub dpad: Dpad,
 }
 
 #[bitsize(4)]
+#[derive(DebugBits, Clone, Copy, FromBits, Default)]
+pub struct UpperNibble {
+    // bilge bitfields are LSB at the top
+    select: NibbleSelect,
+    reserved: u2,
+}
+
+#[bitsize(2)]
+#[derive(Debug, Clone, Copy, FromBits, Default)]
+pub enum NibbleSelect {
+    Button = 0b01,
+    Dpad = 0b10,
+    #[fallback]
+    #[default]
+    Reserved,
+}
+
+#[bitsize(4)]
 #[derive(DebugBits, Clone, Copy, Default)]
 pub struct Buttons {
-    pub start: bool,
-    pub select: bool,
-    pub b: bool,
     pub a: bool,
+    pub b: bool,
+    pub select: bool,
+    pub start: bool,
 }
 
 #[bitsize(4)]
 #[derive(DebugBits, Clone, Copy, Default)]
 pub struct Dpad {
-    pub down: bool,
-    pub up: bool,
-    pub left: bool,
     pub right: bool,
+    pub left: bool,
+    pub up: bool,
+    pub down: bool,
 }
 
 impl Joypad {
-    pub const fn write_joypad(&mut self, value: u8) {
+    pub fn write_joypad(&mut self, value: u8) {
         // lower nibble is read-only
-        let mask = 0b1111_0000;
-
-        self.register |= value & mask;
+        self.input_select = UpperNibble::from(u4::extract_u8(value, 4));
     }
 
-    // TODO: Switch to <https://docs.rs/bilge/latest/bilge/>?
+    // FIXME: Implement proper reading for if both buttons/dpad is selected
+    // "The good news is you can actually select both buttons and directions by setting both
+    // selection bits low. The resulting bits will be low if either the corresponding direction or
+    // button is pressed."
+    // <https://www.reddit.com/r/EmuDev/comments/zq6ygz/comment/j0yo0uh/>
     pub fn read_joypad(self) -> u8 {
-        let upper = self.register & 0b1111_0000;
-        let lower = match (self.register >> 4) & 0b11 {
-            0b01 => self.button_nibble(),
-            0b10 => self.dpad_nibble(),
-            0b11 => 0xF,
-            0b00 => todo!("buttons and dpad selected for joypad read"),
-            _ => unreachable!(),
+        let upper: u8 = u4::from(self.input_select).into();
+        let lower = match self.input_select.select() {
+            NibbleSelect::Button => self.button_nibble(),
+            NibbleSelect::Dpad => self.dpad_nibble(),
+            NibbleSelect::Reserved => {
+                todo!(
+                    "handle incorrect joypad selection bits: {:04b}",
+                    self.input_select.value
+                );
+            }
         };
-        upper | lower
+        upper << 4 | lower
     }
 
     fn button_nibble(self) -> u8 {
